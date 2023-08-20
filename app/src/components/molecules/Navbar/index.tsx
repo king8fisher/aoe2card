d1eed949bceb0b1f0fa737574c8e028b7857b834
king8fisher <140904760+king8fisher@users.noreply.github.com>
2023-08-20 14:36:39 -0400

    Fix lost hidden state

--- app/src/components/molecules/Navbar/index.tsx
+++ app/src/components/molecules/Navbar/index.tsx
@@ -14,7 +14,7 @@ const Navbar = ({ search, setSearch, runDebouncer }: NavbarProps): JSX.Element =
       <a href="/" className="shrink-0 p-[0.4rem] rounded-md bg-gradient-to-b from-zinc-100/20 to-zinc-100/50 dark:from-zinc-900/20 dark:to-zinc-900/50">
         <div className="flex flex-row flex-wrap gap-1 items-center">
           <img src="favicon.png" className="w-6 h-6 shrink-0" />
-          <span className="text-center text-sm first-letter:hidden font-extralight leading-none md:block opacity-40">Aoe2<br/>Card</span>
+          <span className="text-center text-sm font-extralight leading-none hidden md:block opacity-40">Aoe2<br/>Card</span>
         </div>
       </a>
       <div className="flex flex-row w-full max-w-[500px] items-center gap-1">
