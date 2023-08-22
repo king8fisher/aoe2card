0ef578803e7643cc9a166b77ed423fc35973eb57
king8fisher <140904760+king8fisher@users.noreply.github.com>
2023-08-22 12:54:17 -0400

    Beginning of a grouped view

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,16 @@
-import { SlButton, SlDropdown, SlMenu, SlMenuItem } from "@shoelace-style/shoelace/dist/react";
+import { SlButton, SlButtonGroup, SlDropdown, SlMenu, SlMenuItem } from "@shoelace-style/shoelace/dist/react";
 import "@shoelace-style/shoelace/dist/themes/dark.css";
 import "@shoelace-style/shoelace/dist/themes/light.css";
 import { useEffect, useMemo, useState } from "react";
-import { IUnitCivData, allCivUnits, allCivs, searchUnits } from "../../data/model";
+import {
+  ICost,
+  IGroupByUnitData,
+  IUnitCivData,
+  allCivUnits,
+  allCivs,
+  groupByUnitType,
+  searchUnits,
+} from "../../data/model";
 import Navbar from "./components/molecules/Navbar";
 import { createDebouncer } from "./helpers/tools";
 import { Container, FlexWrap, UnitDisplayLine, UnitDisplayLineItemsCentered, UnitsPresentationFlex } from "./styles";
@@ -23,6 +31,9 @@ function App() {
   //   }
   // }, []);
 
+  const [groupedView, setGroupedView] = useState(true);
+  const [civView, setCivView] = useState(false);
+
   const searchDebouncer = createDebouncer();
   const { destroyDebouncer, runDebouncer } = searchDebouncer || {};
   useEffect(() => {
@@ -31,59 +42,134 @@ function App() {
     };
   });
 
+  interface ISearchResult {
+    grouped: IGroupByUnitData[];
+    units: IUnitCivData[];
+  }
+
   const [search, setSearch] = useState("");
-  const [searchResult, setSearchResult] = useState<IUnitCivData[]>();
+  const [searchResult, setSearchResult] = useState<ISearchResult>();
   const unitsByCiv: IUnitCivData[] = useMemo(() => {
     return allCivUnits(selectedCivKey);
   }, [selectedCivKey]);
 
   useMemo(() => {
-    const result = searchUnits(search);
-    setSearchResult(result);
+    let units = searchUnits(search);
+    setSearchResult({
+      units: units,
+      grouped: groupByUnitType(units),
+    });
   }, [search]);
 
   return (
     <>
       <Navbar search={search} setSearch={setSearch} runDebouncer={runDebouncer} />
       <Container>
-        <UnitsPresentationFlex>
-          {searchResult?.map((v, _index) => (
-            <UnitPresentation key={`${v.civ.key}-${v.unit.id}`} unitCivData={v} showCivName={true} />
-          ))}
-        </UnitsPresentationFlex>
-        <SlDropdown className="shadow-lg">
-          <SlButton slot="trigger" caret>
-            <img slot="prefix" src={civImgUrl(selectedCivKey)} className="w-5 h-5 flex-shrink-0" />
-            {selectedCivKey}
+        <SlButtonGroup className="mt-2">
+          <SlButton size="small" variant={groupedView ? "primary" : "default"} onClick={(_) => setGroupedView(true)}>
+            Grouped
           </SlButton>
-          <SlMenu
-            onSlSelect={(event) => {
-              setCiv(event.detail.item.value);
-            }}
-          >
-            {civsList.map((value) => (
-              <SlMenuItem key={value.key} value={value.key}>
-                {value.value}
-                <img slot="prefix" src={civImgUrl(value.key)} className="w-5 h-5 flex-shrink-0" />
-              </SlMenuItem>
+          <SlButton size="small" variant={!groupedView ? "primary" : "default"} onClick={(_) => setGroupedView(false)}>
+            Linear
+          </SlButton>
+        </SlButtonGroup>
+
+        <SlButtonGroup className="mt-2 ml-2">
+          <SlButton size="small" variant={civView ? "primary" : "default"} onClick={(_) => setCivView(!civView)}>
+            Civ
+          </SlButton>
+        </SlButtonGroup>
+        {groupedView ? (
+          <UnitsPresentationFlex>
+            {searchResult?.grouped.map((v, _index) => (
+              <GroupedUnitPresentation key={`${v.unit.id}`} groupByUnitData={v} />
             ))}
-          </SlMenu>
-        </SlDropdown>
+          </UnitsPresentationFlex>
+        ) : (
+          <UnitsPresentationFlex>
+            {searchResult?.units.map((v, _index) => (
+              <UnitPresentation key={`${v.civ.key}-${v.unit.id}`} unitCivData={v} showCivName={true} />
+            ))}
+          </UnitsPresentationFlex>
+        )}
       </Container>
 
-      <Container>
-        <UnitsPresentationFlex>
-          {unitsByCiv?.map((v, _index) => (
-            <UnitPresentation key={`${v.civ.key}-${v.unit.id}`} unitCivData={v} showCivName={false} />
-          ))}
-        </UnitsPresentationFlex>
-      </Container>
+      {civView ? (
+        <Container>
+          <SlDropdown className="shadow-lg">
+            <SlButton slot="trigger" caret>
+              <img slot="prefix" src={civImgUrl(selectedCivKey)} className="w-5 h-5 flex-shrink-0" />
+              {selectedCivKey}
+            </SlButton>
+            <SlMenu
+              onSlSelect={(event) => {
+                setCiv(event.detail.item.value);
+              }}
+            >
+              {civsList.map((value) => (
+                <SlMenuItem key={value.key} value={value.key}>
+                  {value.value}
+                  <img slot="prefix" src={civImgUrl(value.key)} className="w-5 h-5 flex-shrink-0" />
+                </SlMenuItem>
+              ))}
+            </SlMenu>
+          </SlDropdown>
+          <UnitsPresentationFlex>
+            {unitsByCiv?.map((v, _index) => (
+              <UnitPresentation key={`${v.civ.key}-${v.unit.id}`} unitCivData={v} showCivName={false} />
+            ))}
+          </UnitsPresentationFlex>
+        </Container>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
 
 function civImgUrl(civKey: string) {
   return `https://aoe2techtree.net/img/Civs/${civKey.toLowerCase()}.png`;
+}
+
+function GroupedUnitPresentation({ groupByUnitData }: { groupByUnitData: IGroupByUnitData }) {
+  let stringifiedCommon = JSON.stringify(groupByUnitData.mostCommonUnitStats.cost);
+  return (
+    <>
+      <div
+        className={[
+          "flex flex-col rounded-md p-1",
+          groupByUnitData.unit.isImperialAgeUniqueUnit
+            ? "bg-blue-400 dark:bg-blue-700"
+            : "bg-zinc-300 dark:bg-zinc-700",
+        ].join(" ")}
+      >
+        <UnitDisplayLineItemsCentered>
+          <img
+            src={`https://aoe2techtree.net/img/Units/${groupByUnitData.unit.id}.png`}
+            className="w-5 h-5 flex-shrink-0 mt-[2px] rounded-md opacity-50 ml-[4px]"
+          />
+          {groupByUnitData.unit.value}
+          <span className="opacity-50 ml-1 text-xs">{groupByUnitData.unit.id}</span>
+        </UnitDisplayLineItemsCentered>
+        <UnitDisplayLine className="text-xs opacity-80 mt-1">
+          <CostPresentation cost={groupByUnitData.mostCommonUnitStats.cost} />
+        </UnitDisplayLine>
+        <div className="grid grid-cols-8 gap-1 p-1 mt-1">
+          {groupByUnitData?.civs.map((c, _index) => (
+            <div className="flex flex-col items-center">
+              <img src={civImgUrl(c.civ.key)} className="w-7 h-7" />
+              {JSON.stringify(c.unitStats.cost) == stringifiedCommon ? (
+                <></>
+              ) : (
+                // Doesn't seem to ever kick in
+                <CostPresentation cost={c.unitStats.cost} />
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 function UnitPresentation({ unitCivData, showCivName }: { unitCivData: IUnitCivData; showCivName: boolean }) {
@@ -112,24 +198,24 @@ function UnitPresentation({ unitCivData, showCivName }: { unitCivData: IUnitCivD
           <span className="opacity-50 ml-1 text-xs">{unitCivData.unit.id}</span>
         </UnitDisplayLineItemsCentered>
         <UnitDisplayLine className="text-xs opacity-80 mt-1">
-          <CostPresentation unitCivData={unitCivData} />
+          <CostPresentation cost={unitCivData.unitStats.cost} />
         </UnitDisplayLine>
       </div>
     </>
   );
 }
 
-function CostPresentation({ unitCivData }: { unitCivData: IUnitCivData }) {
-  const shouldShowFoodCost = unitCivData.unitStats.cost.food > 0;
-  const shouldShowWoodCost = unitCivData.unitStats.cost.wood > 0;
-  const shouldShowGoldCost = unitCivData.unitStats.cost.gold > 0;
-  const shouldShowStoneCost = unitCivData.unitStats.cost.stone > 0;
+function CostPresentation({ cost }: { cost: ICost }) {
+  const shouldShowFoodCost = cost.food > 0;
+  const shouldShowWoodCost = cost.wood > 0;
+  const shouldShowGoldCost = cost.gold > 0;
+  const shouldShowStoneCost = cost.stone > 0;
   return (
     <FlexWrap>
-      {shouldShowFoodCost && <SingleCostPresenter type="f" amount={unitCivData.unitStats.cost.food} />}
-      {shouldShowWoodCost && <SingleCostPresenter type="w" amount={unitCivData.unitStats.cost.wood} />}
-      {shouldShowGoldCost && <SingleCostPresenter type="g" amount={unitCivData.unitStats.cost.gold} />}
-      {shouldShowStoneCost && <SingleCostPresenter type="s" amount={unitCivData.unitStats.cost.stone} />}
+      {shouldShowFoodCost && <SingleCostPresenter type="f" amount={cost.food} />}
+      {shouldShowWoodCost && <SingleCostPresenter type="w" amount={cost.wood} />}
+      {shouldShowGoldCost && <SingleCostPresenter type="g" amount={cost.gold} />}
+      {shouldShowStoneCost && <SingleCostPresenter type="s" amount={cost.stone} />}
     </FlexWrap>
   );
 }
