58ceaaa9a9aab1089c619393573ff790325753fb
cnohall <christopher@goodpairdays.com>
2023-08-24 09:07:52 +0900

    remove unnecessare fragment

--- src/App.tsx
+++ src/App.tsx
@@ -137,37 +137,35 @@ const unitImgUrl = (unitId: number) => `https://aoe2techtree.net/img/Units/${uni
 const GroupedUnitPresentation = ({ groupByUnitData }: { groupByUnitData: IGroupByUnitData }) => {
   let commonCostKey = groupByUnitData.mostCommonUnitStats.cost.toKey();
   return (
-    <>
-      <div className={["flex flex-col rounded-md p-1", styleForUnit(groupByUnitData.unit)].join(" ")}>
-        <UnitLine unit={groupByUnitData.unit} />
-        <UnitDisplayLine className="text-xs mt-1">
-          <CostPresentation cost={groupByUnitData.mostCommonUnitStats.cost} />
-        </UnitDisplayLine>
-        <div className="grid grid-cols-8 gap-1 p-1 mt-1">
-          {groupByUnitData?.civs.map((c, _index) => (
-            <div key={`${c.civ.key}`}>
-              <SlTooltip style={{ ["--show-delay" as any]: "400" }}>
-                <div className="flex flex-col gap-1" slot="content">
-                  <span className="font-bold leading-6">{c.civ.value}</span>
-                  <span dangerouslySetInnerHTML={{ __html: c.civ.help }} />
-                </div>
-                <div className="flex flex-col items-center">
-                  <img src={civImgUrl(c.civ.key)} className="w-7 h-7" />
-                  {c.unitStats.cost.toKey() == commonCostKey ? (
-                    <></>
-                  ) : (
-                    // TODO: Doesn't seem to ever kick in
-                    <UnitDisplayLine className="text-xs mt-1">
-                      <CostPresentation cost={c.unitStats.cost} />
-                    </UnitDisplayLine>
-                  )}
-                </div>
-              </SlTooltip>
-            </div>
-          ))}
-        </div>
+    <div className={["flex flex-col rounded-md p-1", styleForUnit(groupByUnitData.unit)].join(" ")}>
+      <UnitLine unit={groupByUnitData.unit} />
+      <UnitDisplayLine className="text-xs mt-1">
+        <CostPresentation cost={groupByUnitData.mostCommonUnitStats.cost} />
+      </UnitDisplayLine>
+      <div className="grid grid-cols-8 gap-1 p-1 mt-1">
+        {groupByUnitData?.civs.map((c, _index) => (
+          <div key={`${c.civ.key}`}>
+            <SlTooltip style={{ ["--show-delay" as any]: "400" }}>
+              <div className="flex flex-col gap-1" slot="content">
+                <span className="font-bold leading-6">{c.civ.value}</span>
+                <span dangerouslySetInnerHTML={{ __html: c.civ.help }} />
+              </div>
+              <div className="flex flex-col items-center">
+                <img src={civImgUrl(c.civ.key)} className="w-7 h-7" />
+                {c.unitStats.cost.toKey() == commonCostKey ? (
+                  <></>
+                ) : (
+                  // TODO: Doesn't seem to ever kick in
+                  <UnitDisplayLine className="text-xs mt-1">
+                    <CostPresentation cost={c.unitStats.cost} />
+                  </UnitDisplayLine>
+                )}
+              </div>
+            </SlTooltip>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
