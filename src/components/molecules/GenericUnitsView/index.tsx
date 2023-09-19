4102a3b8e772ce7e60d640eb34966271196a86bd
cnohall <christopher@goodpairdays.com>
2023-09-19 17:14:22 +0900

    Remove SlDetails for unitView

--- src/components/molecules/GenericUnitsView/index.tsx
+++ src/components/molecules/GenericUnitsView/index.tsx
@@ -1,4 +1,3 @@
-import { SlDetails, SlIcon } from "@shoelace-style/shoelace/dist/react";
 import { UnitsPresentationFlex } from "../../../styles";
 import { GroupedUnitPresentation } from "../GroupedUnitPresentation";
 import { ISearchResult } from "../../../App";
@@ -9,13 +8,9 @@ interface IGenericUnitsViewProps {
 
 const GenericUnitsView = ({ genericUnitsData }: IGenericUnitsViewProps) =>
   !!genericUnitsData?.grouped.length && (
-    <SlDetails className="sl-details w-full mt-2" open>
-      <SlIcon name="plus-square" slot="expand-icon" />
-      <SlIcon name="dash-square" slot="collapse-icon" />
-      <UnitsPresentationFlex>
-        {genericUnitsData?.grouped.map((v, _index) => <GroupedUnitPresentation key={v.unit.id} groupByUnitData={v} />)}
-      </UnitsPresentationFlex>
-    </SlDetails>
+    <UnitsPresentationFlex>
+      {genericUnitsData?.grouped.map((v, _index) => <GroupedUnitPresentation key={v.unit.id} groupByUnitData={v} />)}
+    </UnitsPresentationFlex>
   );
 
 export default GenericUnitsView;
