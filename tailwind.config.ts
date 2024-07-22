ba478e5ba68a09bdbad9b37e221232a69a88c1f5
king8fisher <140904760+king8fisher@users.noreply.github.com>
2024-07-22 14:58:19 -0400

    update dependencies, convert tailwind js to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: "class",
   content: ["index.html", "./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 };
