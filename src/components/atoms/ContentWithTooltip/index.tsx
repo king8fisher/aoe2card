f13f47c5bb979f2d880436f9f2ee4768fee3260d
king8fisher <140904760+king8fisher@users.noreply.github.com>
2024-12-07 06:37:32 -0500

    fix popup appearance

--- src/components/atoms/ContentWithTooltip/index.tsx
+++ src/components/atoms/ContentWithTooltip/index.tsx
@@ -27,7 +27,9 @@ export const ContentWithTooltip = (props: React.PropsWithChildren<TooltipProps>)
 
       </PopoverTrigger>
       <PopoverContent>
-        {props.tooltip}
+        <div className="flex flex-col gap-2 text-sm">
+          {props.tooltip}
+        </div>
       </PopoverContent>
     </Popover>
   );
