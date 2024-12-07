ccf83a51b55a5f0091f344119ace6db2cf201628
king8fisher <140904760+king8fisher@users.noreply.github.com>
2024-12-07 08:58:35 -0500

    return manual invocation of tooltip

--- src/components/atoms/ContentWithTooltip/index.tsx
+++ src/components/atoms/ContentWithTooltip/index.tsx
@@ -1,4 +1,4 @@
-import { type JSX } from "react";
+import { useRef, useState, type JSX } from "react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "~/src/shadcn/components/ui/tooltip";
 
 interface TooltipProps {
@@ -6,10 +6,25 @@ interface TooltipProps {
 }
 
 export const ContentWithTooltip = (props: React.PropsWithChildren<TooltipProps>) => {
+  const [open, setOpen] = useState(false);
+
+  const timerRef = useRef<NodeJS.Timeout>(null); // Ref to store the timeout ID
+
+  const handleMouseEnter = () => {
+    timerRef.current = setTimeout(() => {
+      setOpen(true);
+    }, 300);
+  };
+
+  const handleMouseLeave = () => {
+    if (timerRef.current) clearTimeout(timerRef.current);
+    setOpen(false);
+  };
+
   return (
     <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>{props.children}</TooltipTrigger>
+      <Tooltip open={open}>
+        <TooltipTrigger onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{props.children}</TooltipTrigger>
         <TooltipContent>
           <div className="flex flex-col gap-2 text-sm max-w-96">
             {props.tooltip}
