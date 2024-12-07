e59846c8838cf61c6ea80e84ab96953bf41e829f
king8fisher <140904760+king8fisher@users.noreply.github.com>
2024-12-07 06:22:29 -0500

    add favicon.ico and others

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,11 @@ export const metadata: Metadata = {
   title: 'AoE2 Card',
   description: 'This is a playground for AoE2 stuff made by a bunch of friends playing the game',
   icons: [
-    { rel: "icon", url: "/favicon.png" },
+    { rel: "icon", "type": "image/x-icon", url: "/favicon.ico" }, // multi-page
+    { rel: "icon", "type": "image/png", url: "/favicon.png", sizes: "128x128" },
+    { rel: "icon", "type": "image/png", url: "/icon-512.png", sizes: "512x512" },
+    { rel: "icon", "type": "image/png", url: "/icon-192.png", sizes: "192x192" },
+    { rel: "icon", "type": "image/png", url: "/apple-icon.png", sizes: "180x180" }, // 24bit
   ]
 }
 
