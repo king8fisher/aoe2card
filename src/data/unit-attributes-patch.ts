132fe2d3ec586f874967d594fc0c019dd2f2b8a5
king8fisher <140904760+king8fisher@users.noreply.github.com>
2024-12-08 12:05:57 -0500

    add unpacked and packed labels to trebuchet

---
+++ src/data/unit-attributes-patch.ts
@@ -0,0 +1,25 @@
+interface IUnitAttributes {
+  unitId: number;
+  name: string;
+}
+
+interface IPatchedUnitAttributes {
+  name: string;
+}
+
+/**
+ * Returns passed {id, name}, or a patched unit id, if the image
+ * is missing and provided by another unit.
+ * @param {id, name}
+ * @returns 
+ */
+export function patchedUnitAttributes({ unitId, name }: IUnitAttributes): IPatchedUnitAttributes {
+  switch (unitId) {
+    case 42: // "TREBU", unpacked trebuchet to attack
+      return { name: name + " (Unpacked)" }; // "PTREB"
+    case 331: // "PTREB", packed trebuchet to move
+      return { name: name + " (Packed)" }; // "PTREB"
+    default:
+      return { name };
+  }
+}
